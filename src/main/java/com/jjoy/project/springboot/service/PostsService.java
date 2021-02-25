c560d1ce8d81024f73e14c7b0df17a83f01f5dc0
jjoy <ekdnlt1995@gmail.com>
2021-02-25 23:06:56 +0900

    2월 25일 스프링부트와 AWS로 혼자 구현하는 웹서비스 공부

--- src/main/java/com/jjoy/project/springboot/service/PostsService.java
+++ src/main/java/com/jjoy/project/springboot/service/PostsService.java
@@ -16,6 +16,7 @@ public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
