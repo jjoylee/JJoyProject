9781ad22f8913540f56a989c0c722956d3ceafb4
jjoy <ekdnlt1995@gmail.com>
2021-02-27 15:49:24 +0900

    2월 27일 스프링부트와 AWS로 혼자 구현하는 웹서비스 공부

---
+++ src/main/java/com/jjoy/project/springboot/config/auth/SessionUser.java
@@ -0,0 +1,19 @@
+package com.jjoy.project.springboot.config.auth;
+
+import com.jjoy.project.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
